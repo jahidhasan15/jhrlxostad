e8e3d09beab35bb8e72225835c8da69d743e073a
jahidhasan <jahidhasanifran69@gmail.com>
2024-07-10 23:14:32 +0600

    new

---
+++ android/app/src/main/kotlin/com/example/ostadjh/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ostadjh
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,36 @@
 import 'package:flutter/material.dart';
 
-/*void main() {
-  runApp(Introapp());
-}
-
-class Introapp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Home'),
-          backgroundColor: Colors.blueAccent,
-        ),
-        body: Center(
-          child: Text(
-            'Hello ostad how are yor',
-            maxLines: 1,
-            textAlign: TextAlign.start,
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-              decoration: TextDecoration.underline,
-              letterSpacing: 1,
-              wordSpacing: 1,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}*/
+// void main() {
+//   runApp(Introapp());
+// }
+//
+// class Introapp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Home'),
+//           backgroundColor: Colors.blueAccent,
+//         ),
+//         body: Center(
+//           child: Text(
+//             'Hello ostad how are yor',
+//             maxLines: 1,
+//             textAlign: TextAlign.start,
+//             style: TextStyle(
+//               fontSize: 16,
+//               fontWeight: FontWeight.bold,
+//               decoration: TextDecoration.underline,
+//               letterSpacing: 1,
+//               wordSpacing: 1,
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 void main() {
   runApp(Introapp());
@@ -59,49 +59,134 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.blueAccent,
         title: Text('Home'),
       ),
-      body: Center(
-        //child: Icon(Icons.tips_and_updates_outlined,
-        // size:100,
-        //color: Colors.blueAccent,
-        // ),
-        //child: Image.asset('assets/images/flutter.jpeg',
-        //width: 200,
-        //height: 200,
-        //fit: BoxFit.cover,
-        //),
-        //child: ElevatedButton(
-        //onPressed: (){
-        //print('press button');
-        //},
-        //child: Text('Notifecation'),
-        //)
-        //child: IconButton(
-        //onPressed: (){
-        //print('Add');
-        //},
-        //icon: Icon(Icons.add_circle),
-        //),
-        //child: TextButton(
-        //onPressed: (){
-        //print('pressd text button');
-        //},
-        //child: Text('text button'),
-        //)
-        //child:GestureDetector(
-        //onTap: (){
-        //print('Singal tap');
-        //},
-        //onDoubleTap: (){
-        //print('double tap');
-        //},
-        //),
-        child: InkWell(
-          onTap: () {
-            print('singal tap');
-          },
-          onDoubleTap: () {
-            print('double tap');
-          },
+      // body: Center(
+      //   //child: Icon(Icons.tips_and_updates_outlined,
+      //   // size:100,
+      //   //color: Colors.blueAccent,
+      //   // ),
+      //   //child: Image.asset('assets/images/flutter.jpeg',
+      //   //width: 200,
+      //   //height: 200,
+      //   //fit: BoxFit.cover,
+      //   //),
+      //   //child: ElevatedButton(
+      //   //onPressed: (){
+      //   //print('press button');
+      //   //},
+      //   //child: Text('Notifecation'),
+      //   //)
+      //   //child: IconButton(
+      //   //onPressed: (){
+      //   //print('Add');
+      //   //},
+      //   //icon: Icon(Icons.add_circle),
+      //   //),
+      //   //child: TextButton(
+      //   //onPressed: (){
+      //   //print('pressd text button');
+      //   //},
+      //   //child: Text('text button'),
+      //   //)
+      //   //child:GestureDetector(
+      //   //onTap: (){
+      //   //print('Singal tap');
+      //   //},
+      //   //onDoubleTap: (){
+      //   //print('double tap');
+      //   //},
+      //   //),
+      //   child: InkWell(
+      //     onTap: () {
+      //       print('singal tap');
+      //     },
+      //     onDoubleTap: () {
+      //       print('double tap');
+      //     },
+      //   ),
+      // ),
+      /*body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text('jahid Hasan'),
+            Text('jahid Hasan'),
+            Text('jahid Hasan'),
+            Text('jahid Hasan'),
+            Text('jahid Hasan'),
+            Text('jahid Hasan'),
+            Text('jahid Hasan'),
+            Text('jahid Hasan'),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('text button'),
+            )
+          ],
+        ),
+      ),*/
+      /*body: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.end,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Text('jahid hasan'),
+            Text('jahid hasan'),
+            Text('jahid hasan'),
+            Text('jahid hasan'),
+            Text('jahid hasan'),
+            Text('jahid hasan'),
+            Text('jahid hasan'),
+            Text('jahid hasan'),
+            Text('jahid hasan'),
+            Text('jahid hasan'),
+            Text('jahid hasan'),
+            Text('jahid hasan'),
+            Text('jahid hasan'),
+            Text('jahid hasan'),
+            Text('jahid hasan'),
+          ],
+        ),
+      ),*/
+      body: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: [
+            Column(
+              children: [
+                Text('jahid Hasan'),
+                Text('jahid Hasan'),
+                Text('jahid Hasan'),
+                Text('jahid Hasan'),
+                Text('jahid Hasan'),
+                Text('jahid Hasan'),
+                Text('jahid Hasan'),
+                Text('jahid Hasan'),
+                Text('jahid Hasan'),
+              ],
+            ),
+            Column(
+              children: [
+                Text('jahid Hasan'),
+                Text('jahid Hasan'),
+                Text('jahid Hasan'),
+                Text('jahid Hasan'),
+                Text('jahid Hasan'),
+                Text('jahid Hasan'),
+              ],
+            ),
+            Column(
+              children: [
+                Text('jahid Hasan'),
+                Text('jahid Hasan'),
+                Text('jahid Hasan'),
+                Text('jahid Hasan'),
+                Text('jahid Hasan'),
+              ],
+            ),
+          ],
         ),
       ),
     );
